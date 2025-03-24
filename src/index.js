1d4b7e22ecd08f1579bc38f86917b0c03395ba02
kxshiii <brian.gikaru@student.moringaschool.com>
2025-03-24 11:16:24 +0300

    commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+
 document.addEventListener('DOMContentLoaded', () => {
   const characterBar = document.getElementById('character-bar');
   const detailedInfo = document.getElementById('detailed-info');
